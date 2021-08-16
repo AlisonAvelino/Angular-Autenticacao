0bbef16a1fe108a194a5f6c308384811a1f23251
Alison Avelino <alisonavelino3133@gmail.com>
2021-08-16 17:57:52 -0300

    Arquivos do projeto de autenticacao em angular

---
+++ src/app/models/dados-usuario.model.spec.ts
@@ -0,0 +1,7 @@
+import { DadosUsuario } from './dados-usuario.model';
+
+describe('DadosUsuario', () => {
+  it('should create an instance', () => {
+    expect(new DadosUsuario()).toBeTruthy();
+  });
+});
